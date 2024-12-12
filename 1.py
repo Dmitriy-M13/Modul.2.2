040824fa7f3c13a12b6cbb4a05dc1a6114f25cd0
Dmitriy-M13 <dimamalyakshin@gmail.com>
2024-12-12 11:32:04 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,16 @@
+week = int(input("Введите номер дня недели от 1 до 7: "))
+
+if week == 1:
+    print('Понедельник')
+if week == 2:
+    print('Вторник')
+if week == 3:
+    print('Среда')
+if week == 4:
+    print('Четверг')
+if week == 5:
+    print('Пятница')
+if week == 6:
+    print('Суббота')
+if week == 7:
+    print('Воскресенье')
